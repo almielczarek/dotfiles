ba9da54832b60efff014ab9d46b48613abbdad85
Alexander Mielczarek <almielczarek@gmail.com>
2020-08-28 21:32:02 -0500

    Add git ignored device.el to doom config
    This file is used to set device specific emacs settings such as font size.

--- doom/.doom.d/config.el
+++ doom/.doom.d/config.el
@@ -169,15 +169,7 @@
     (exwm-input--fake-key ?\C-v)))
 
 ;; (require 'org-drill)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (gpastel))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
+(let ((device-specific-config "~/.doom.d/device.el"))
+  (when (file-exists-p device-specific-config)
+    (load-file device-specific-config)))
