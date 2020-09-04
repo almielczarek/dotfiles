5ee918765895e40b60feb574b13f70d1ad72251a
Alexander Mielczarek <almielczarek@gmail.com>
2020-09-04 11:25:44 -0500

    Set doom-henna theme

--- doom/.doom.d/config.el
+++ doom/.doom.d/config.el
@@ -21,10 +21,12 @@
 ;; font string. You generally only need these two:
 (setq doom-font (font-spec :family "Source Code Pro" :size 12))
 
+(custom-set-faces! '(org-headline-done :foreground "#606F73"))
+
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'flatui)
+(setq doom-theme 'doom-henna)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
